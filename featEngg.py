72e05021fe65e35cd8465b81d00eb48e4bd9fe31
krishnasripada <krishna.sripada@colorado.edu>
2015-04-21 17:25:46 -0600

    With features and MSE 6404

--- featEngg.py
+++ featEngg.py
@@ -5,10 +5,19 @@ from sklearn import tree
 import yaml
 from time import gmtime, strftime
 from sklearn.metrics import mean_squared_error
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.svm import SVR
 from sklearn import linear_model
-from sklearn.ensemble import AdaBoostRegressor
+from nltk.tokenize import WordPunctTokenizer
+from collections import defaultdict
+from collections import Counter
+from math import sqrt
+import ast
+from nltk.corpus import wordnet as wn
+from itertools import product
+import nltk
+from pattern.en import tag
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
 
 class classify:
     
@@ -31,13 +40,66 @@ class classify:
 
     def questionFeatures(self):
         questions = list(DictReader(open("questions.csv", 'rU')))
+        self.train = list(DictReader(open("train.csv", 'rU')))
+        positions = {}
+        for each in self.train:
+            positions.update({each['question']: float(each['position'])})
+
         for ques in questions:
             unigrams = {}
             unigrams['_length_'] = len(ques['questionText'])
             unigrams['_cat_'] = ques['cat']
             unigrams['_answer_'] = ques['answer']
-            self.quesFeat[ques['question']] = unigrams     
-            
+            unigrams['_answerLength_'] = len(ques['answer'])
+            bestWord = ""
+            bestPos = 0
+            for pos, word in ast.literal_eval(ques['unigrams']).items():
+                if ques['question'] in positions.keys():
+                    
+                    #Best word closest to the question position
+                    if pos<=positions[ques['question']]:
+                        bestWord = word
+                        bestPos = pos
+                    
+                    ###Number and Year feature
+                    try:
+                        if isinstance(int(str(word)), int):
+                            if len(str(word)) <= 3:
+                                if "_number_" in unigrams.keys():
+                                    unigrams["_number_"]+=1
+                                else:
+                                    unigrams["_number_"]= 1
+                            else:
+                                if "_year_" in unigrams.keys():
+                                    unigrams["_year_"]+=1
+                                else:
+                                    unigrams["_year_"]=1
+
+                    except:
+                        None
+                
+                    ## Just the nouns
+                    for w,t in tag(str(word)):
+                        if str(t) in ['NN', 'NNS', 'NNP']:
+                            if "_nouns_" in unigrams.keys():
+                                unigrams["_nouns_"]+=1
+                            else:
+                                unigrams["_nouns_"]=1
+        
+                    ### Lower case and stemming unigrams
+                    stemmed_word = stemmer.stem(str(word).lower())
+                    if stemmed_word in unigrams:
+                        unigrams[stemmed_word]+=1
+                    else:
+                        unigrams[stemmed_word]=1
+                            
+                    #### Stemming countries with removed punctuation
+
+        
+            unigrams['_bestWord_'] = bestWord
+            unigrams['_bestWordPos_'] = bestPos
+            self.quesFeat[ques['question']] = unigrams
+
     def readData(self, accuracy):
         print "In readData"
         self.train = list(DictReader(open("train.csv", 'rU')))
@@ -49,9 +111,8 @@ class classify:
         for each in self.train:
             features = {}
             features = self.quesFeat[each['question']]
-            features['_user_'] = each['user']
-            features['_question_'] = each['question']
-            #features['_answer_'] = each['answer']
+            #features['_user_'] = each['user']
+            #features['_question_'] = each['question']
             
             if(accuracy==False):
                 self.x_train.append(features)
@@ -82,7 +143,7 @@ class classify:
                 
     def predict(self):
         print "In Predict"
-        clf = linear_model.Lasso(alpha=0.01, fit_intercept=True)
+        clf = linear_model.Lasso(alpha=0.01)
         clf = clf.fit(self.x_train, self.y)
         self.predictions = clf.predict(self.x_test)
 
@@ -97,9 +158,8 @@ class classify:
     def getAccuracy(self):
         
         print "In getAccuracy"
-        #clf = tree.DecisionTreeClassifier()
         
-        clf = linear_model.Lasso(alpha=0.01, fit_intercept=True)
+        clf = linear_model.Lasso(alpha=0.01)
         clf = clf.fit(self.cv_train, self.cv_y_train)
         self.predictions = clf.predict(self.cv_test)
         
